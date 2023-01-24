10665a377b6a50148d5f7ccd0a0e69edcf72437b
penciless <binh.ng1195@gmail.com>
2023-01-24 07:24:20 +0700

    update relative urls

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 // Binh.settings.url_comp = '/comp/';
-Binh.settings.url_comp = 'http://localhost:1302/components/';
-Binh.settings.url_serv = 'http://localhost:1302/services/';
+Binh.settings.url_comp = '/components/';
+Binh.settings.url_serv = '/services/';
 
 Binh.settings.buildComponentURL = function(script_name, resolve) {
     return Binh.settings.url_comp + script_name + '.js';
